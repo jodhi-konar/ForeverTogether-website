37839154422088c00f5ee920f8182c14f039d1d4
my cat <jodhikonar3@gmail.com>
2024-12-07 20:51:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth animations for the message
+document.addEventListener("DOMContentLoaded",() => {
+  const message = document.getElementById("love-message");
+  message.innerText = "This website was created by your pondatti pa...🤭🫣!";
+});
+
+
